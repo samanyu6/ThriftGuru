1cd204558c42ffb1be145ed390bd72471efae417
therubbishnetwork <therubbishnetwork@gmail.com>
2019-08-12 12:04:51 +0530

    done

--- android/app/src/main/java/com/thriftguru/MainApplication.java
+++ android/app/src/main/java/com/thriftguru/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import iyegoroff.RNTextGradient.RNTextGradientPackage;
 import com.facebook.react.ReactNativeHost;
